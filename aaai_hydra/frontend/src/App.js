4d08f78e7cd36c50f360061a9cd18cc9c825abe4
wjingyao <wjingyao97@gmail.com>
2021-09-05 12:12:34 +0800

    frontend

---
+++ aaai_hydra/frontend/src/App.js
@@ -0,0 +1,24 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="center-column">
+        <div className="item-row">
+          <span>TEST 1</span>
+        </div>
+
+        <div className="item-row">
+          <span>TEST 2</span>
+        </div>
+
+        <div className="item-row">
+          <span>Test 3</span>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
