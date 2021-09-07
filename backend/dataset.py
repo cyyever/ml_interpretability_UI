aef4ea340fcbf89a6d3976d0f00f6a2734bf19cc
cyy <cyyever@outlook.com>
2021-09-07 16:40:16 +0800

    add code

--- backend/dataset.py
+++ backend/dataset.py
@@ -1,6 +1,7 @@
 import typing
 
 from cyy_torch_toolbox.dataset_collection import DatasetCollection
+from cyy_torch_toolbox.ml_type import DatasetType, MachineLearningPhase
 
 
 def get_supported_dataset_names() -> typing.Sequence[str]:
@@ -9,3 +10,12 @@ def get_supported_dataset_names() -> typing.Sequence[str]:
 
 def get_dataset_collection(name: str) -> DatasetCollection:
     return DatasetCollection.get_by_name(name)
+
+
+def list_data(dc: DatasetCollection, phase: MachineLearningPhase):
+    if dc.dataset_type == DatasetType.Vision:
+        dataset_util = dc.get_dataset_util(phase)
+        for i in range(len(dataset_util)):
+            yield (dataset_util.get_sample_image(i), dataset_util.get_sample_label(i))
+        return
+    raise RuntimeError("Unimplemented Code")
