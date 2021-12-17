07cc9b8f57932dc13f92f426f92348302b46b80d
unknown <wjingyao97@gmail.com>
2021-12-17 14:45:28 +0800

    fix

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,13 +7,21 @@ import 'react-bootstrap-table-next/dist/react-bootstrap-table2.min.css';
 import {Container , Row} from 'react-bootstrap'
 import InputPanel from './components/InputPanel';
 import OutputPanel from './components/OutputPanel';
+import React, { useState } from 'react';
 
 function App() {
+
+  const[datasetId , setDataId] = useState(0);
+
+  function updateDatasetId(datasetId){
+    setDataId(datasetId);
+  }
+
   return (
     <div className="App">
       <Container fluid className = "h-100">
         <Row className="h-100">
-          <InputPanel/>
+          <InputPanel updateDatasetId = {updateDatasetId}/>
           <OutputPanel/>
         </Row>
       </Container>
