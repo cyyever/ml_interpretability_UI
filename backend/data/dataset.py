a7858c5fd6bd6da83dcc965c765039453f64ce62
cyy <cyyever@outlook.com>
2021-12-21 21:07:40 +0800

    add code

--- backend/data/dataset.py
+++ backend/data/dataset.py
@@ -4,16 +4,22 @@ from cyy_torch_toolbox.dataset_collection import DatasetCollection
 from cyy_torch_toolbox.ml_type import MachineLearningPhase
 
 
-class dataset(): 
+class dataset:
     def get_supported_dataset_names() -> typing.Sequence[str]:
         return DatasetCollection.get_dataset_constructors().keys()
-
 
     def get_dataset_collection(name: str) -> DatasetCollection:
         return DatasetCollection.get_by_name(name)
 
+    def get_dataset_size(dc: DatasetCollection, phase: MachineLearningPhase):
+        return len(dc.get_dataset(phase))
 
-    def get_raw_data_from_dataset(dc: DatasetCollection, phase: MachineLearningPhase) -> typing.Sequence[tuple]:
+    def generate_raw_data_from_dataset(
+        dc: DatasetCollection, phase: MachineLearningPhase
+    ) -> typing.Sequence[tuple]:
         return dc.generate_raw_data(phase)
 
-
+    def get_raw_data_from_dataset(
+        dc: DatasetCollection, phase: MachineLearningPhase, index: int
+    ) -> tuple:
+        return dc.get_raw_data_from_dataset(phase, index)
