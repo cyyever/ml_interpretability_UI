e9181921d50e9c927498cd8f293cdb8704d52038
cyy <cyyever@outlook.com>
2021-12-25 16:07:25 +0800

    fix code

--- backend/data/dataset.py
+++ backend/data/dataset.py
@@ -15,6 +15,9 @@ class dataset:
         labels = dc.get_dataset_util(phase=phase).split_by_label()
         return {str(k): v["indices"] for k, v in labels.items()}
 
+    def get_label_set(dc: DatasetCollection) -> list:
+        return sorted(dc.get_labels())
+
     def get_raw_data_from_dataset(
         dc: DatasetCollection, phase: MachineLearningPhase, index: int
     ) -> tuple:
@@ -24,17 +27,17 @@ class dataset:
     #     return len(dc.get_dataset(phase))
 
     def generate_raw_data_from_dataset(
-         dc: DatasetCollection, phase: MachineLearningPhase
-     ) -> typing.Sequence[tuple]:
-         return dc.generate_raw_data(phase)
+        dc: DatasetCollection, phase: MachineLearningPhase
+    ) -> typing.Sequence[tuple]:
+        return dc.generate_raw_data(phase)
 
-''' test driver'''
 
-#if __name__ == "__main__":
+""" test driver"""
+
+# if __name__ == "__main__":
 #    dataset_ = dataset.get_label_indices(dataset.get_dataset_collection("CIFAR10"),1)
 #    indexes = dataset_.get("9")
 #    return_data = []
 #    for x in list(indexes):
 #        result = list(dataset.get_raw_data_from_dataset(dataset.get_dataset_collection("CIFAR10") , 1, x))
 #        return_data.append(result)
-   
