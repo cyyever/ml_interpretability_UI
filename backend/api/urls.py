ac5981ccc1e84a876f0bc94c37993e7cd219a124
unknown <wjingyao97@gmail.com>
2021-12-27 01:38:34 +0800

    fix

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 
-from .views import (Dataset_v2View, DatasetLabelIndexView, DatasetLabelView,
+from .views import (RawDataView, DatasetLabelIndexView, DatasetLabelView,
                     DatasetNameView)
 
 urlpatterns = [
