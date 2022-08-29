f6ae65a594cb06b196777a9137728bc19e680038
cyy <cyyever@outlook.com>
2022-08-29 14:12:56 +0800

    fix code

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,19 +1,27 @@
-from getopt import getopt
 from django.urls import path
 
-from .views import (RawDataView, DatasetLabelIndexView, DatasetLabelView, getModelView ,
-                    DatasetNameView ,startRunModelView , getModelResultView , getOptimizersView , getLearningRateSchedulerView , getContributionResultView)
+from .views import (DatasetLabelIndexView, DatasetLabelView, DatasetNameView,
+                    RawDataView, getContributionResultView,
+                    getLearningRateSchedulerView, getModelResultView,
+                    getModelView, getOptimizersView, startRunModelView)
 
 urlpatterns = [
     path("api/datasetName", DatasetNameView.as_view(), name="dataset name"),
     path("api/datasetLabel", DatasetLabelView.as_view(), name="dataset label"),
     path("api/datasetLabelIndices", DatasetLabelIndexView.as_view(), name="dataset"),
     path("api/rawData", RawDataView.as_view(), name="dataset"),
-    path("api/getModel" ,getModelView.as_view(), name = "model name"),
-    path("api/startRunModel" ,startRunModelView.as_view(), name = "start model"),
-    path("api/getModelResult" ,getModelResultView.as_view(), name = "get model result "),
-    path("api/getOptimizers", getOptimizersView.as_view(), name = "optimizers name"),
-    path("api/getLearningRateScheduler" , getLearningRateSchedulerView.as_view() , name = "learning rate scheduler name"),
-    path("api/getContributionResult" , getContributionResultView.as_view() , name = "learning contribution result"),
-    
+    path("api/getModel", getModelView.as_view(), name="model name"),
+    path("api/startRunModel", startRunModelView.as_view(), name="start model"),
+    path("api/getModelResult", getModelResultView.as_view(), name="get model result "),
+    path("api/getOptimizers", getOptimizersView.as_view(), name="optimizers name"),
+    path(
+        "api/getLearningRateScheduler",
+        getLearningRateSchedulerView.as_view(),
+        name="learning rate scheduler name",
+    ),
+    path(
+        "api/getContributionResult",
+        getContributionResultView.as_view(),
+        name="learning contribution result",
+    ),
 ]
--- backend/test/test_dataset.py
+++ backend/test/test_dataset.py
@@ -1,9 +1,10 @@
 #!/usr/bin/env python3
 
-from cyy_torch_toolbox.dataset_collection import DatasetCollection
+from cyy_torch_toolbox.dataset_collection import (
+    ClassificationDatasetCollection, create_dataset_collection)
 from cyy_torch_toolbox.ml_type import MachineLearningPhase
 
 
 def test_dataset():
-    mnist = DatasetCollection.get_by_name("MNIST")
+    mnist = create_dataset_collection(ClassificationDatasetCollection, "MNIST")
     assert next(mnist.generate_raw_data(MachineLearningPhase.Training))
